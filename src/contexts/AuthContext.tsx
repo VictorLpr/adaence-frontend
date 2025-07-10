d37079b403c9503b71f6092537fd2d0110dd55f5
VictorLpr <victorlepron@gmail.com>
2025-07-10 10:00:00 +0200

    update cookie length to match server side cookie

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -49,11 +49,11 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
     return null
   }
 
-  const setCookie = (name: string, value: string, days: number = 7) => {
+  const setCookie = (name: string, value: string) => {
     if (typeof document === 'undefined') return
 
     const expires = new Date()
-    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000)
+    expires.setTime(expires.getTime() + 60 * 60 * 1000)
     document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=Strict`
   }
 
